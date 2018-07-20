688954d645eadb0b87918938a6c5a4662feedd78
mamanberliansyah <jungman.nurdin@gmail.com>
2018-07-20 16:02:13 +0700

    Reupload the whole dir

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
   "fmt"
   "net/http"
+  "github.com/gorilla/mux"
 )
 
 func main(){
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+  router := mux.NewRouter()
+  router.HandleFunc("/index", handler).Methods("GET")
+  http.ListenAndServe(":8080", router)
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
