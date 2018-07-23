00f58fc2b273d882781dc454f2c386ae6305f06f
mamanberliansyah <jungman.nurdin@gmail.com>
2018-07-23 14:55:11 +0700

    added config for Port & public_html

--- src/main.go
+++ src/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
   "net/http"
+  "fmt"
+   "strconv"
   "github.com/gorilla/mux"
+  "github.com/BurntSushi/toml"
 )
 
-func newRouter() *mux.Router {
+type Config struct {
+  Enable  bool
+  Port    int
+  Dir     string
+}
+
+func newRouter(dir string) *mux.Router {
   router := mux.NewRouter()
-  staticFileDirectory := http.Dir("./assets/")
+  staticFileDirectory := http.Dir(dir)
   staticFileHandler := http.StripPrefix("/", http.FileServer(staticFileDirectory))
   router.PathPrefix("/").Handler(staticFileHandler).Methods("GET")
   return router
 }
 
 func main(){
-  router := newRouter()
-  http.ListenAndServe(":8080", router)
+  var conf Config
+  if _, err := toml.DecodeFile("./config/config.toml", &conf); err != nil {
+    fmt.Println(err)
+  }
+  if (conf.Enable){
+    router := newRouter(conf.Dir)
+    port := ":" + strconv.Itoa(conf.Port)
+    http.ListenAndServe(port, router)
+  }
 }
